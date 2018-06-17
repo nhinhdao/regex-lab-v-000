aeb56846deb9412da08aec6adc14ba0168ec8ef8
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-17 03:05:08 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,8 @@
 def starts_with_a_vowel?(word)
-      word.scan(/\b[aeiouAEIOU]/) ? true : false
+      if word.scan(/\b[aeiouAEIOU]/)
+            true
+      elsif word.scan(/\b^[aeiouAEIOU]/) 
+             false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)

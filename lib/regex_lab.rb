34cc6f7a42d872805e1419b8fb31032a06712929
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-17 03:29:44 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,5 +20,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-      phone.scan(/.*\d{3}.*\d{3}.*\d{4}$/) ? true : false
+      # phone.scan(/.*\d{3}.*\d{3}.*\d{4}$/) ? true : false
+      phone.scan(/^.?([0-9]{3}).?([0-9]{3}).?([0-9]{4})$/) ? true : false
 end

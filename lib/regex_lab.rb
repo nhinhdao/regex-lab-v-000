e795cf60aaf7fc2f68c684e4150d3828132ee6ba
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-17 03:53:41 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-      word.scan(/\b[aeiouAEIOU]/) ? true : false
+      word.scan(/\b[aeiouAEIOU][a-z]*\b/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
